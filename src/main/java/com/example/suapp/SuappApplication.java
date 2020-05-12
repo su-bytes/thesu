90c6d923da82e71799ddc0560512bda0991d7fb6
su-bytes <53877987+su-bytes@users.noreply.github.com>
2020-05-12 13:28:13 +0530

    Add files via upload

---
+++ src/main/java/com/example/suapp/SuappApplication.java
@@ -0,0 +1,13 @@
+package com.example.suapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuappApplication.class, args);
+	}
+
+}
